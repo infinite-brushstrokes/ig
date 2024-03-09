bae4218b577c82ceb0def5823ec51fbc9eea3b79
Vishavjeet <vishavjeetsharma88@gmail.com>
2024-03-09 22:19:18 +0530

    change

--- app.py
+++ app.py
@@ -14,14 +14,26 @@ collection = db['imageIds']
 def hello_world():
     return f'{mongo_uri}'
 
+@app.route('/delete/<id>')
+def delete_data(id):
+    data = collection.find_one({'id': id})
+    if data:
+        result = collection.delete_one({'id': id})
+        if result.deleted_count == 1:
+            return jsonify({ 'id' : data['id'], 'caption': data['caption']})
+        else:
+            return jsonify({})
+    else:
+        return jsonify({})
+
 @app.route('/add/<id>')
-def add_data(id, caption):
+def add_data(id):
     data = {'id': id, 'caption': "#abstractart"}
     collection.insert_one(data)
     return jsonify({'id': id, 'caption': caption})
 
 @app.route('/add/<id>/<caption>')
-def add_data_caption(id, caption):
+def add_data_caption(id, caption="#abstract"):
     data = {'id': id, 'caption': caption }
     collection.insert_one(data)
     return jsonify({'id': id, 'caption': caption})
