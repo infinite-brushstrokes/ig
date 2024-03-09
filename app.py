a9f92b2d4209faae77f8e6c7d3820193edf2d770
Vishavjeet <vishavjeetsharma88@gmail.com>
2024-03-09 19:52:39 +0530

    change

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
+mongo_uri = os.getenv("MONGO_URI")
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World changed!'
+    return f'{mongo_uri}'
